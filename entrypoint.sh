8e31935d6e02406df3666611f8da760587341a55
Stephen Tallowitz <stephen.tallowitz@petzi.ch>
2018-01-14 15:33:47 +0100

    Indentation

--- entrypoint.sh
+++ entrypoint.sh
@@ -85,24 +85,24 @@ function prepare_proxy_variables(){
 	# to the domain localhost.
 	if [ ! -z ${PROXY_MODE} ] && [ ${PROXY_MODE} = "dev" ]
 	then
-	    echo_warn "Running in dev mode. Will use self-signed certificates."
-	    echo_warn "Not recommended for integration and production setup."
-	    echo_warn "Only localhost will be used as a host name."
+		echo_warn "Running in dev mode. Will use self-signed certificates."
+		echo_warn "Not recommended for integration and production setup."
+		echo_warn "Only localhost will be used as a host name."
 	else
-	    if [ -z ${PROXY_CERTBOT_MAIL} ]
-	    then
-	        echo_err "PROXY_CERTBOT_MAIL is not set. It is required for letsencrypt"
-	        exit 1
-	    fi
+		if [ -z ${PROXY_CERTBOT_MAIL} ]
+		then
+			echo_err "PROXY_CERTBOT_MAIL is not set. It is required for letsencrypt"
+			exit 1
+		fi
 	fi
 
 	# PROXY_BACKEND should be set.
 	if [ -z ${PROXY_BACKEND} ]
 	then
-	    echo_warn "PROXY_BACKEND is not set."
-	    echo_warn "The hostname 'localhost' will be used."
-	    echo_warn "This will most likely cause errors."
-	    export PROXY_BACKEND="localhost"
+		echo_warn "PROXY_BACKEND is not set."
+		echo_warn "The hostname 'localhost' will be used."
+		echo_warn "This will most likely cause errors."
+		export PROXY_BACKEND="localhost"
 	fi
 
 	# Default values for some variables
@@ -136,107 +136,107 @@ function create_acme_challenge_dir(){
 
 # Set basic_auth in the global http section
 function set_basic_auth(){
-if [ ! -z ${PROXY_AUTH_USER} ]
-then
-	echo_info "Setting HTTP basic protection (user: $PROXY_AUTH_USER)"
-	# Create the file first
-	echo "$PROXY_AUTH_USER:{PLAIN}$PROXY_AUTH_PASSWORD" > /etc/nginx/conf.d/auth_basic.inc
-	# Then configure auth_basic in the HTTP section
-	if [ `grep -c "auth_basic" $nginx_conf` -lt 1 ]
+	if [ ! -z ${PROXY_AUTH_USER} ]
 	then
-		# First time
-		sed -i '/^http/a \auth_basic "test";' $nginx_conf
-		sed -i '/^auth_basic\ /a \auth_basic_user_file /etc/nginx/conf.d/auth_basic.inc;' $nginx_conf
-	else
-		sed -i 's/^auth_basic.*$\ /auth_basic "test";/' $nginx_conf
-		sed -i 's/^auth_basic_user_file.*$\ /auth_basic_user_file /etc/nginx/conf.d/auth_basic.inc;/' $nginx_conf
+		echo_info "Setting HTTP basic protection (user: $PROXY_AUTH_USER)"
+		# Create the file first
+		echo "$PROXY_AUTH_USER:{PLAIN}$PROXY_AUTH_PASSWORD" > /etc/nginx/conf.d/auth_basic.inc
+		# Then configure auth_basic in the HTTP section
+		if [ `grep -c "auth_basic" $nginx_conf` -lt 1 ]
+		then
+			# First time
+			sed -i '/^http/a \auth_basic "test";' $nginx_conf
+			sed -i '/^auth_basic\ /a \auth_basic_user_file /etc/nginx/conf.d/auth_basic.inc;' $nginx_conf
+		else
+			sed -i 's/^auth_basic.*$\ /auth_basic "test";/' $nginx_conf
+			sed -i 's/^auth_basic_user_file.*$\ /auth_basic_user_file /etc/nginx/conf.d/auth_basic.inc;/' $nginx_conf
+		fi
 	fi
-fi
 }
 
 # Create configuration files for HTTP mode
 function create_config_files_http(){
-echo_debug "Generating nginx configuration files for http mode"
-$envsubst_cmd < /etc/nginx/conf.d/http_default_backend.conf.orig > /etc/nginx/conf.d/http_default_backend.conf
-$envsubst_cmd < /etc/nginx/conf.d/http_default.conf.orig > /etc/nginx/conf.d/http_default.conf
+	echo_debug "Generating nginx configuration files for http mode"
+	$envsubst_cmd < /etc/nginx/conf.d/http_default_backend.conf.orig > /etc/nginx/conf.d/http_default_backend.conf
+	$envsubst_cmd < /etc/nginx/conf.d/http_default.conf.orig > /etc/nginx/conf.d/http_default.conf
 }
 
 # Generate a certificate
 function generate_certificate(){
-# In PROXY_MODE dev generate self-signed using openssl
-# In any other mode, generate using certbot
-if [ ! -z ${PROXY_MODE} ] && [ ${PROXY_MODE} = "dev" ]
-then
-    mkdir -p $le_path
-    if [ -f $le_privkey ]
-    then
-	echo_warn "Private key already exists, not overwriting ($le_privkey)"
-    else
-	echo_info "Generating self-signed certificate"
-        openssl req -subj $le_dev_subject -x509 -sha256 -newkey rsa:1024 -nodes -keyout $le_privkey -out $le_fullchain -days 365
-    fi
-elif [ -f "$le_fullchain" ]
-then
-    echo_info "Renewing certificate"
-    certbot renew
-else
-    # certbot is run with the following options:
-    # certonly (installing in nginx is not yet supported)
-    # -n        = non-interactive (this is a script, after all)
-    # --webroot = webroot method to /var/www/html
-    # -d        = domain is passed as ENV variable
-    # --agree-tos   = agree to terms of service (non-interactive)
-    # -m            = mail address for letsencrypt account
-    # --keep        = do not replace existing certificate, unless expiry is close
-    echo_info "Generating certificate"
-    certbot certonly -n --webroot -w /var/www/html -d ${PROXY_DOMAIN} --agree-tos -m ${PROXY_CERTBOT_MAIL} --keep
-fi
+	# In PROXY_MODE dev generate self-signed using openssl
+	# In any other mode, generate using certbot
+	if [ ! -z ${PROXY_MODE} ] && [ ${PROXY_MODE} = "dev" ]
+	then
+		mkdir -p $le_path
+		if [ -f $le_privkey ]
+		then
+			echo_warn "Private key already exists, not overwriting ($le_privkey)"
+		else
+			echo_info "Generating self-signed certificate"
+			openssl req -subj $le_dev_subject -x509 -sha256 -newkey rsa:1024 -nodes -keyout $le_privkey -out $le_fullchain -days 365
+		fi
+	elif [ -f "$le_fullchain" ]
+	then
+		echo_info "Renewing certificate"
+		certbot renew
+	else
+		# certbot is run with the following options:
+		# certonly (installing in nginx is not yet supported)
+		# -n        = non-interactive (this is a script, after all)
+		# --webroot = webroot method to /var/www/html
+		# -d        = domain is passed as ENV variable
+		# --agree-tos   = agree to terms of service (non-interactive)
+		# -m            = mail address for letsencrypt account
+		# --keep        = do not replace existing certificate, unless expiry is close
+		echo_info "Generating certificate"
+		certbot certonly -n --webroot -w /var/www/html -d ${PROXY_DOMAIN} --agree-tos -m ${PROXY_CERTBOT_MAIL} --keep
+	fi
 }
 
 # Some setup after certificate generation
 function post_certificate_setup(){
-$envsubst_cmd < /etc/nginx/conf.d/http_default_ssl.conf.orig > /etc/nginx/conf.d/http_default_ssl.conf
+	$envsubst_cmd < /etc/nginx/conf.d/http_default_ssl.conf.orig > /etc/nginx/conf.d/http_default_ssl.conf
 
-# Redirect to https port
-sed -i "s/^.*return.*$/        return 301 https:\/\/\$server_name:${PROXY_HTTPS_PORT}\$request_uri;/" /etc/nginx/conf.d/http_default.conf
+	# Redirect to https port
+	sed -i "s/^.*return.*$/        return 301 https:\/\/\$server_name:${PROXY_HTTPS_PORT}\$request_uri;/" /etc/nginx/conf.d/http_default.conf
 }
 
 # Create the entries for static files
 function create_static_files_entries(){
-# Create the entries for static files
-stat="/etc/nginx/conf.d/default_static_dirs.conf.inc"
-echo "" > $stat
-for i in $PROXY_STATIC_DIRS
-do
-	location=`echo $i | awk -F"," '{print $1}'`
-	directory=`echo $i | awk -F"," '{print $2}'`
-    echo_info "Creating static entry: $location -> $directory"
-    echo "location /$location/ { root $directory; }" >> $stat
-done
+	# Create the entries for static files
+	stat="/etc/nginx/conf.d/default_static_dirs.conf.inc"
+	echo "" > $stat
+	for i in $PROXY_STATIC_DIRS
+	do
+		location=`echo $i | awk -F"," '{print $1}'`
+		directory=`echo $i | awk -F"," '{print $2}'`
+		echo_info "Creating static entry: $location -> $directory"
+		echo "location /$location/ { root $directory; }" >> $stat
+	done
 }
 
 # Copy extra configuration (mostly from derived image)
 function prepare_extraconf(){
-if [ -d /extraconf ]
-then
-	echo_info "Copying /extraconf to /etc/nginx/conf.d"
-	cp /extraconf/* /etc/nginx/conf.d/
-	cd /etc/nginx/conf.d/
-	for i in `ls -1 stream_*.conf.orig ssl_*.conf.inc.orig`; do
-		output_filename=`echo $i | rev | cut -c 6- | rev`
-		echo_info "Replacing env vars: $i -> $output_filename"
-		$envsubst_cmd < $i > $output_filename
-	done
-	cd
-fi
+	if [ -d /extraconf ]
+	then
+		echo_info "Copying /extraconf to /etc/nginx/conf.d"
+		cp /extraconf/* /etc/nginx/conf.d/
+		cd /etc/nginx/conf.d/
+		for i in `ls -1 stream_*.conf.orig ssl_*.conf.inc.orig`; do
+			output_filename=`echo $i | rev | cut -c 6- | rev`
+			echo_info "Replacing env vars: $i -> $output_filename"
+			$envsubst_cmd < $i > $output_filename
+		done
+		cd
+	fi
 }
 
 # Copy extrahtml
 function copy_extrahtml(){
-if [ -d /extrahtml ]
-then
-	cp -a /extrahtml/* /var/www/html/
-fi
+	if [ -d /extrahtml ]
+	then
+		cp -a /extrahtml/* /var/www/html/
+	fi
 }
 
 echo_with_prefix "(Nginx-Letsencrypt) starting entrypoint.sh"
