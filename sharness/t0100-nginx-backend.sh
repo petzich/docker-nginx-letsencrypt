f77ad123ba09b3e419866fd81dbddf820177f8c7
Stephen Tallowitz <stephen.tallowitz@petzi.ch>
2018-03-18 22:29:26 +0100

    Correct test to properly reflect output

--- sharness/t0100-nginx-backend.sh
+++ sharness/t0100-nginx-backend.sh
@@ -11,8 +11,8 @@ expected="server backend1 max_fails=3 fail_timeout=5s max_conns=100;"
 test_expect_success "Single backend" '
 	export PROXY_BACKENDS="backend1"
 	export PROXY_TUNING_UPSTREAM_MAX_CONNS=100
-	echo "$expected" >e1 &&
-	echo "$(nginx_cfg_backend_string_core)" >a1 &&
+	echo $expected >e1 &&
+	echo $(nginx_cfg_backend_string_core) >a1 &&
 	test_cmp e1 a1
 '
 
@@ -21,8 +21,8 @@ server backend2 max_fails=3 fail_timeout=5s max_conns=50;"
 test_expect_success "Two backends" '
 	export PROXY_BACKENDS="backend1 backend2"
 	export PROXY_TUNING_UPSTREAM_MAX_CONNS=50
-	echo "$expected" >e2 &&
-	echo "$(nginx_cfg_backend_string_core)" >a2 &&
+	echo $expected >e2 &&
+	echo $(nginx_cfg_backend_string_core) >a2 &&
 	test_cmp e2 a2
 '
 
@@ -32,8 +32,8 @@ server backend3 max_fails=3 fail_timeout=5s max_conns=50;"
 test_expect_success "Three backends" '
 	export PROXY_BACKENDS="backend1 backend2 backend3"
 	export PROXY_TUNING_UPSTREAM_MAX_CONNS=50
-	echo "$expected" >e3 &&
-	echo "$(nginx_cfg_backend_string_core)" >a3 &&
+	echo $expected >e3 &&
+	echo $(nginx_cfg_backend_string_core) >a3 &&
 	test_cmp e3 a3
 '
 
@@ -46,8 +46,8 @@ server backend3 max_fails=3 fail_timeout=5s max_conns=50;
 test_expect_success "Complete string" '
 	export PROXY_BACKENDS="backend1 backend2 backend3"
 	export PROXY_TUNING_UPSTREAM_MAX_CONNS=50
-	echo "$expected" >e-complete &&
-	echo "$(nginx_cfg_backend_string)" >a-complete &&
+	echo $expected >e-complete &&
+	echo $(nginx_cfg_backend_string) >a-complete &&
 	test_cmp e-complete a-complete
 '
 
