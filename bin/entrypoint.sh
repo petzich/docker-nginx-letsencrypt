b27e2f0775dca561f4debc711296aa3ea25540f6
Stephen Tallowitz <stephen.tallowitz@petzi.ch>
2018-03-18 23:01:16 +0100

    Move console / debug output to library

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -21,52 +21,7 @@ le_dev_subject="/C=CH/ST=BE/L=Berne/O=ExampleOrg/OU=Org/CN=localhost"
 envsubst_cmd=
 nginx_conf="/etc/nginx/nginx.conf"
 
-# Script should echo all output in the same manner
-function echo_with_prefix(){
-	echo "$echo_prefix $1"
-}
-function echo_with_loglevel(){
-	echo_loglevel="[$1]"
-	shift;
-	echo_message="$@"
-	echo_with_prefix "$echo_loglevel $echo_message"
-}
-# Use this level if the script has to exit with an error
-function echo_error(){
-	echo_with_loglevel "ERROR" $1
-}
-# Use this level if a user should configure an environment variable,
-# but hasn't done so. Or if setting an environment variable to a certain
-# value has unexpected consequences for the user.
-function echo_warn(){
-	if [ "$ENTRYPOINT_LOGLEVEL" -ge 2 ]
-	then
-		echo_with_loglevel "WARNING" $1
-	fi
-}
-# Use this level to output relevant information on user configuration a
-# derived image.
-function echo_info(){
-	if [ "$ENTRYPOINT_LOGLEVEL" -ge 3 ]
-	then
-		echo_with_loglevel "info" $1
-	fi
-}
-# Use this level to output internal information on this script
-function echo_debug(){
-	if [ "$ENTRYPOINT_LOGLEVEL" -ge 4 ]
-	then
-		echo_with_loglevel "debug" $1
-	fi
-}
-
-# Prepare the loglevel
-function prepare_loglevel(){
-	if [ -z ${ENTRYPOINT_LOGLEVEL} ]
-	then
-		ENTRYPOINT_LOGLEVEL=3
-	fi
-}
+. $libdir/_entrypoint_debug.sh
 
 # Prepare environment variables and set up defaults
 function prepare_proxy_variables(){
