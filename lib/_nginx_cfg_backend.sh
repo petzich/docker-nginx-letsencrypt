c9b841bcb3a704b18788550bdb3ecbbdffd9c35c
Stephen Tallowitz <stephen.tallowitz@petzi.ch>
2018-03-28 22:28:58 +0200

    Bugfix: prioritise correct output of whole config section

--- lib/_nginx_cfg_backend.sh
+++ lib/_nginx_cfg_backend.sh
@@ -10,13 +10,12 @@ nginx_cfg_backend_string_core () {
 		retval="${retval}server ${backend} max_fails=3 fail_timeout=5s max_conns=${PROXY_TUNING_UPSTREAM_MAX_CONNS};"
 		counter=$(expr $counter + 1)
 	done
-	echo -en $retval
+	echo $retval
 }
 
 nginx_cfg_backend_string () {
-	backend_string_core="$(nginx_cfg_backend_string_core)"
 	retval="upstream backend_server {\n"
 	retval="${retval}ip_hash;\n"
-	retval="${retval}${backend_string_core}\n}"
+	retval="${retval}$(nginx_cfg_backend_string_core)\n}"
 	echo -en $retval
 }
