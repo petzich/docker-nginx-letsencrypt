2b16a88194fc27cf73b17250c800cb8158af9b8d
Stephen Tallowitz <stephen.tallowitz@petzi.ch>
2018-04-04 18:43:27 +0200

    Add tests for https default section

---
+++ test/_test_nginx_cfg_https.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+
+. ${libdir}/_nginx_cfg_https.sh
+
+setUp(){
+	unset PROXY_DOMAIN
+	unset PROXY_HTTP_PORT
+	unset PROXY_HTTPS_PORT
+}
+
+# Test the https default section
+testHttpsDefault(){
+	export PROXY_DOMAIN="ssl.example.org"
+	export PROXY_HTTPS_PORT="5443"
+	expected="
+server {
+  listen 5443;
+  ssl on;
+  ssl_certificate     /etc/letsencrypt/live/ssl.example.org/fullchain.pem;
+  ssl_certificate_key /etc/letsencrypt/live/ssl.example.org/privkey.pem;
+
+  include conf.d/default_static_dirs.conf.inc;
+
+  location / {
+    proxy_pass http://backend_server/;
+    proxy_set_header Host ssl.example.org;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+
+    # Extension point for derived images
+    include conf.d/ssl_*.conf.inc;
+  }
+
+  # redirect server error pages to the static page /50x.html
+  #
+  error_page   500 502 503 504  /50x.html;
+  location = /50x.html {
+    auth_basic off;
+    root   /usr/share/nginx/html;
+  }
+}"
+	actual=$(nginx_cfg_https_default)
+	assertEquals "$expected" "$actual"
+}
+
+. ${extlibdir}/shunit2/shunit2
