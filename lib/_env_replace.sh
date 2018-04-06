99b7727a951b96061f25ad535c1dff92375102da
Stephen Tallowitz <stephen.tallowitz@petzi.ch>
2018-04-07 00:05:55 +0200

    Add method to replace variables with shell script

---
+++ lib/_env_replace.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# Replace environment variables in a file
+# This is a public API
+# Parameters:
+# 1. Input filename
+# 2. Output filename
+# 3. Whitelist of environment variables. Use quoting.
+env_replace_in_file() {
+	inFile=$1
+	outFile=$2
+	varlist=$3
+	if [ -f $inFile ]
+	then
+		inString=$(cat $inFile)
+		echo "inString: $inString"
+		outString=$(env_replace_in_string "$inString" "$varlist")
+		echo "outString: $outString"
+		echo $outString > $outFile
+	else
+		echo "ERROR: inFile $inFile not found"
+	fi
+}
+
+# Replace environment variables in a string
+# This is a private API
+# Parameters
+# 1. Input variable (containing a string)
+# 2. Whitelist of environment variables. Separated by <space>.
+# Output: the string with the variables replaced
+env_replace_in_string() {
+	local input=$1
+	local varlist=$2
+	# Initialise output with input
+	local output=$input		
+	local v
+	for v in $varlist
+	do
+		local var_clean=$(env_name_cleanup $v)
+		local input="${output}"
+		if [ ! -z \$$var_clean ]
+		then
+			eval val="\$$var_clean"
+			# First process curly braces
+			local sed_string_curly="s/\\\${$var_clean}/$val/g"
+			local output_curly=$(echo $input | sed $sed_string_curly)
+			# Then process without curly braces
+			local sed_string_short="s/\\\$$var_clean/$val/g"
+			local output_short=$(echo $output_curly | sed $sed_string_short)
+			local output=$output_short
+		fi
+	done
+	echo "$output"
+}
+
+# Restrict the name of environment variables to [a-zA-Z0-9_]
+# Parameters:
+# 1. environment variable name
+env_name_cleanup(){
+	local retval=$(echo "$1" | sed -e 's/[^a-zA-Z0-9_]//g')
+	echo "$retval"
+}
---
+++ test/_test_env_replace.sh
@@ -0,0 +1,65 @@
+#! /bin/sh
+
+. ${libdir}/_env_replace.sh
+inputFile=/tmp/testInput.txt
+outputFile=/tmp/testOutput.txt
+
+setUp(){
+	if [ -f $inputFile ]; then rm $inputFile; fi
+	if [ -f $outputFile ]; then rm $outputFile; fi
+	unset a1 b2 c3 d4
+}
+
+testEnvNameCleanup(){
+	input="a-zA-Z0-9_ "
+	expected="azAZ09_"
+	actual=$(env_name_cleanup $input)
+	assertEquals "$expected" "$actual"
+}
+
+testEnvNameCleanupHarder(){
+	input="a-zA-Z0-9_ ,{}äöüçœ!^``'?"
+	expected="azAZ09_"
+	actual=$(env_name_cleanup $input)
+	assertEquals "$expected" "$actual"
+}
+
+testEnvReplaceString(){
+	input='Hello ${a1}, ${b2}, $c3 and $d4'
+	export a1="one"
+	export b2="two"
+	export c3="three"
+	export d4="four"
+	actual=$(env_replace_in_string "$input" "a1 b2 c3 d4")
+	assertEquals "Hello one, two, three and four" "$actual"
+	actual=$(env_replace_in_string "$input" "a1 c3")
+	assertEquals "Hello one, \${b2}, three and \$d4" "$actual"
+	actual=$(env_replace_in_string "$input" "a2")
+	assertEquals "Hello \${a1}, \${b2}, \$c3 and \$d4" "$actual"
+	actual=$(env_replace_in_string "$input" "")
+	assertEquals "Hello \${a1}, \${b2}, \$c3 and \$d4" "$actual"
+	input='Hello ${a1} and $a1'
+	actual=$(env_replace_in_string "$input" "a1")
+	assertEquals "Hello one and one" "$actual"
+}
+
+testEnvReplaceFileSimple(){
+	export a1="one" b2="two" c3="three" d4="four"
+	echo 'Hello ${a1}, ${b2}, $c3 and $d4.' > $inputFile
+	env_replace_in_file $inputFile $outputFile "a1 b2 c3 d4"
+	expected='Hello one, two, three and four.'
+	actual=$(cat "$outputFile")
+	assertEquals "$expected" "$actual"
+}
+
+testEnvReplaceFileSomeMissing() {
+	export a1="one"
+	echo -e 'Hello ${a1}, ${b2}, $a1 and $b2' > $inputFile
+	env_replace_in_file $inputFile $outputFile "a1"
+	expected="Hello one, \${b2}, one and \$b2"
+	actual=$(cat "$outputFile")
+	assertEquals "$expected" "$actual"
+}
+
+. ${extlibdir}/shunit2/shunit2
+
