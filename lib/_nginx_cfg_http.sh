a938c078ff556b5c7caaa3c8b6894d06ccca6f64
Stephen Tallowitz <stephen.tallowitz@petzi.ch>
2018-04-11 07:13:27 +0200

    Use parameters for main http section

--- lib/_nginx_cfg_http.sh
+++ lib/_nginx_cfg_http.sh
@@ -1,26 +1,38 @@
 #!/bin/sh
 
+# Generate redirect to https
+# Parameters
+# $1: https_port
 nginx_cfg_http_https_redirect() {
+	local https_port=$1
 	retval="    location / {
-        return 302 https://\$server_name:${PROXY_HTTPS_PORT}\$request_uri;
+        return 302 https://\$server_name:$https_port\$request_uri;
     }
 "
 	echo "$retval"
 }
 
+# Generate default http section
+# Parameters:
+# $1: domainname
+# $2: http_port
+# $3: https_port
 nginx_cfg_http_default() {
+	local domain=$1
+	local http_port=$2
+	local https_port=$3
 	retval="server {
     auth_basic off;
-    listen ${PROXY_HTTP_PORT};
+    listen $http_port;
     ssl off;
-    server_name ${PROXY_DOMAIN};
+    server_name $domain;
 
     location /.well-known/ {
         auth_basic off;
         root /var/www/html;
     }
 
-$(nginx_cfg_http_https_redirect)
+$(nginx_cfg_http_https_redirect $https_port)
 
     # redirect server error pages to the static page /50x.html
     #
--- test/_test_nginx_cfg_http.sh
+++ test/_test_nginx_cfg_http.sh
@@ -2,27 +2,17 @@
 
 . ${libdir}/_nginx_cfg_http.sh
 
-setUp(){
-	unset PROXY_DOMAIN
-	unset PROXY_HTTP_PORT
-	unset PROXY_HTTPS_PORT
-}
-
 # Test the redirect string
 testHttpHttpsRedirect(){
-	export PROXY_HTTPS_PORT="12345"
 	expected="    location / {
         return 302 https://\$server_name:12345\$request_uri;
     }"
-	actual=$(nginx_cfg_http_https_redirect)
+	actual=$(nginx_cfg_http_https_redirect 12345)
 	assertEquals "$expected" "$actual"
 }
 
 # Test the default http configuration
 testHttpDefault(){
-	export PROXY_DOMAIN="test.example.org"
-	export PROXY_HTTP_PORT="8080"
-	export PROXY_HTTPS_PORT="8443"
 	expected="server {
     auth_basic off;
     listen 8080;
@@ -46,7 +36,7 @@ testHttpDefault(){
         root   /usr/share/nginx/html;
     }
 }"
-	actual=$(nginx_cfg_http_default)
+	actual=$(nginx_cfg_http_default test.example.org 8080 8443)
 	assertEquals "$expected" "$actual"
 }
 
